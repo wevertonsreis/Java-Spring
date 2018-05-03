58d92e08f5e7ff5fa647d460fad32557ae4ba5b6
Weverton Reis <weverton.sant.reis@gmail.com>
2018-05-03 15:12:44 -0300

    treino spring boot

---
+++ logistica/src/main/java/br/com/logistica/logistica/LogisticaApplication.java
@@ -0,0 +1,12 @@
+package br.com.logistica.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaApplication.class, args);
+	}
+}
